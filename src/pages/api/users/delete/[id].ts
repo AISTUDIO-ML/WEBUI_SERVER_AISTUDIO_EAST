31bb7f438c7967a538aad8eeb2a1ba9bb5d13fc4
Muhammad Waqas Rizvi <76169327+Waqas-Rizvi@users.noreply.github.com>
2024-02-18 01:33:20 +0500

    remove another error

--- src/pages/api/users/delete/[id].ts
+++ src/pages/api/users/delete/[id].ts
@@ -1,26 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import User from "src/models/user.model"
+import User from 'src/models/user.model'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: any, res: any) {
   // Handle the API request and response
 
-  if(req.method === 'GET'){
-    const data = { message: 'This is a GET request' };
-    res.status(200).json(data);
-  }else if( req.method === "DELETE") {
-    
-    
-    const { id } = req.query;
+  if (req.method === 'GET') {
+    const data = { message: 'This is a GET request' }
+    res.status(200).json(data)
+  } else if (req.method === 'DELETE') {
+    const { id } = req.query
     console.log(id)
-    try{
-      await User.deleteOne({_id: id})
+    try {
+      await User.deleteOne({ _id: id })
       res.status(200).json({
         status: true
       })
-    } catch(err){
-      console.log(err);
+    } catch (err) {
+      console.log(err)
       res.status(200).json({
-        status: false,
+        status: false
       })
     }
   }
