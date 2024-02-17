68f75984202114730c06566f721a93d7ca5ec99d
Muhammad Waqas Rizvi <76169327+Waqas-Rizvi@users.noreply.github.com>
2024-02-18 01:23:36 +0500

    remove NextApiResponse

--- src/pages/api/users/add-user.ts
+++ src/pages/api/users/add-user.ts
@@ -1,26 +1,25 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import User from "src/models/user.model"
+import User from 'src/models/user.model'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: any, res: any) {
   // Handle the API request and response
 
-  if(req.method === 'GET'){
-    const data = { message: 'This is a GET request' };
-    res.status(200).json(data);
-  }else if( req.method === "POST") {
-    console.log("=============================")
+  if (req.method === 'GET') {
+    const data = { message: 'This is a GET request' }
+    res.status(200).json(data)
+  } else if (req.method === 'POST') {
+    console.log('=============================')
     console.log(req.body)
-    try{
+    try {
       const user = await User.create(req.body)
       res.status(200).json({
         status: true,
         user: user
       })
-    } catch(err){
-      console.log(err);
+    } catch (err) {
+      console.log(err)
       res.status(200).json({
         status: false,
-        message: "Creating a new user is failed."
+        message: 'Creating a new user is failed.'
       })
     }
   }
--- src/pages/api/users/edit-user.ts
+++ src/pages/api/users/edit-user.ts
@@ -1,25 +1,24 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import User from "src/models/user.model"
+import User from 'src/models/user.model'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: any, res: any) {
   // Handle the API request and response
 
-  if(req.method === 'GET'){
-    const data = { message: 'This is a GET request' };
-    res.status(200).json(data);
-  }else if( req.method === "POST") {
+  if (req.method === 'GET') {
+    const data = { message: 'This is a GET request' }
+    res.status(200).json(data)
+  } else if (req.method === 'POST') {
     console.log(req.body)
-    try{
-      const user = await User.updateOne({_id: req.body._id}, { ...req.body})
+    try {
+      const user = await User.updateOne({ _id: req.body._id }, { ...req.body })
       res.status(200).json({
         status: true,
         user: user
       })
-    } catch(err){
-      console.log(err);
+    } catch (err) {
+      console.log(err)
       res.status(200).json({
         status: false,
-        message: "Creating a new user is failed."
+        message: 'Creating a new user is failed.'
       })
     }
   }
